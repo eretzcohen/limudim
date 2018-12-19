a800067fcdf2465dc1a8de3c736011e384b9f60a
eretz <ereth111@gmail.com>
2018-12-19 18:26:35 +0200

    edited note

--- storegit/storegit/Controllers/HomeController.cs
+++ storegit/storegit/Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace storegit.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Awesome application";
 
             return View();
         }
